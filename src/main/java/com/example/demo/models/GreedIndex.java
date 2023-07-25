912fdc14318e03f962f3dc905f83dac664797fb7
root <root@DESKTOP-OVBKLFB>
2023-07-25 12:19:52 -0500

    front end was missing

--- src/main/java/com/example/demo/models/GreedIndex.java
+++ src/main/java/com/example/demo/models/GreedIndex.java
@@ -1,15 +1,15 @@
 package com.example.demo.models;
 
-@Entity
-@Table(name = "users")
-public class GreedIndex {
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long id;
+// @Entity
+// @Table(name = "users")
+// public class GreedIndex {
+//     @Id
+//     @GeneratedValue(strategy = GenerationType.IDENTITY)
+//     private Long id;
  
-    private String firstName;
-    private String lastName;
-    private String email;
+//     private String firstName;
+//     private String lastName;
+//     private String email;
  
-    // getters and setters
-}
+//     // getters and setters
+// }
